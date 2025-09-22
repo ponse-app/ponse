2129a11d0590df9d53cc7fc3258007f1b926aea6
Atte Oinonen <atteaoinonen@gmail.com>
2025-09-22 13:37:46 +0300

    Added leaflet dependency

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 var map = L.map('map').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+/* L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 L.marker([51.5, -0.09]).addTo(map)
     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-    .openPopup();
+    .openPopup(); */
 
 export default function Home() {
   return (
