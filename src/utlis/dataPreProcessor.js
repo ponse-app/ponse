a806316c813998729ebe3b822c1134b1d4f80ebb
Juuso Turunen <44379448+juuso-turunen@users.noreply.github.com>
2025-11-12 12:53:58 +0200

    Fix: calculate vakimaara only in main map, and in previewMap use directly value from data

--- src/utlis/dataPreProcessor.js
+++ src/utlis/dataPreProcessor.js
@@ -13,6 +13,7 @@ const preProcessData = (features, parameter) => {
   const equivalencyTable = {
     miehet: "he_miehet",
     naiset: "he_naiset",
+    vakimaara: "he_vakiy",
   };
 
   if (features[0].properties.hasOwnProperty(equivalencyTable[parameter])) {
