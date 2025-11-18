9430cffd12f9c5f3d344a05b0da590728387e3b7
Juuso Turunen <44379448+juuso-turunen@users.noreply.github.com>
2025-11-18 18:16:31 +0200

    Add parameters to parameter selector
    Co-authored-by: Joonas Paasovaara <paajooni@student.jyu.fi>

--- src/components/DataSelector/ParameterSelector.js
+++ src/components/DataSelector/ParameterSelector.js
@@ -12,6 +12,10 @@ const Map = ({ inputName, setParameter }) => {
                 <option value="miehet">Miesten määrä</option>
                 <option value="naiset">Naisten määrä</option>
                 <option value="ika_65_p">ika_65_p</option>
+                <option value="ika_13_17">ika_13_17</option>
+                <option value="ika_13_17_p">ika_13_17_p</option>
+                <option value="ika_18_24">ika_18_24</option>
+                <option value="ika_13_24">ika_13_24</option>
                 <option value="vaesto">Väkimäärä</option>
                 <option value="ra_ke">Kesämökit yhteensä *</option>
                 <option value="pt_opisk">Päätoimiset opiskelijat yhteensä *</option>
