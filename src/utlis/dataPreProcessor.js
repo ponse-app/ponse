f8eac66b9777f6fdb3072ec038c0a52343a28154
Juuso Turunen <44379448+juuso-turunen@users.noreply.github.com>
2025-11-18 18:14:35 +0200

    Change implementation of pre-processing and added support for mode advanced calculations
    Co-authored-by: Joonas Paasovaara <paajooni@student.jyu.fi>

--- src/utlis/dataPreProcessor.js
+++ src/utlis/dataPreProcessor.js
@@ -3,8 +3,12 @@ import pno_stat from "@/app/pno_tilasto_2024.json";
 const preProcessData = (features, parameter) => {
   if (features.length === 0) return;
 
+  return features.map((feature) => processData(feature, parameter));
+};
+
+const processData = (feature, parameter) => {
   // If parameter is found from features, then nothing is needed to do
-  if (features[0].properties.hasOwnProperty(parameter)) return features;
+  if (feature.properties.hasOwnProperty(parameter)) return feature;
 
   // Try find corresponding parameter with mapping
   const equivalencyTable = {
@@ -13,16 +17,14 @@ const preProcessData = (features, parameter) => {
     vaesto: "he_vakiy",
   };
 
-  if (features[0].properties.hasOwnProperty(equivalencyTable[parameter])) {
-    return features.map((feature) => {
-      return {
-        ...feature,
-        properties: {
-          ...feature.properties,
-          [parameter]: feature.properties[equivalencyTable[parameter]],
-        },
-      };
-    });
+  if (feature.properties.hasOwnProperty(equivalencyTable[parameter])) {
+    return {
+      ...feature,
+      properties: {
+        ...feature.properties,
+        [parameter]: feature.properties[equivalencyTable[parameter]],
+      },
+    };
   }
 
   // Calculate properties based on these options
@@ -31,59 +33,85 @@ const preProcessData = (features, parameter) => {
       parameters: ["miehet", "naiset"],
       operator: "+",
     },
+    ika_13_17: {
+      parameters: ["he_13_15", "he_16_17"],
+      operator: "+",
+    },
+    ika_18_24: {
+      parameters: ["he_18_19", "he_20_24"],
+      operator: "+",
+    },
+    ika_13_24: {
+      parameters: ["ika_13_17", "ika_18_24"],
+      operator: "+",
+    },
+    ika_13_17_p: {
+      parameters: ["ika_13_17", "vaesto"],
+      operator: "/",
+    },
   };
 
   const calc = (feature) => {
     switch (calculationMap[parameter].operator) {
       case "+":
         const sum = calculationMap[parameter].parameters.reduce((sum, cur) => {
-          if (feature.properties.hasOwnProperty(cur))
-            return feature.properties[cur] + sum;
-
-          if (feature.properties.hasOwnProperty(equivalencyTable[cur]))
-            return feature.properties[equivalencyTable[cur]] + sum;
+          return processData(feature, cur).properties[cur] + sum;
         }, 0);
         return sum;
+
+      case "/":
+        const [firstCalculatedParameter, secondCalculatedParameter] =
+          calculationMap[parameter].parameters;
+
+        // TODO: ei haluta aina kertoa sadalla, vaan ainoastaan jakaa.
+        // Pitää siis luoda erillinen kertolaskuoperaatio
+        return (
+          (processData(feature, firstCalculatedParameter).properties[
+            firstCalculatedParameter
+          ] /
+            processData(feature, secondCalculatedParameter).properties[
+              secondCalculatedParameter
+            ]) *
+          100
+        );
     }
   };
 
   if (calculationMap.hasOwnProperty(parameter)) {
-    return features.map((feature) => {
-      return {
-        ...feature,
-        properties: {
-          ...feature.properties,
-          [parameter]: calc(feature),
-        },
-      };
-    });
+    return {
+      ...feature,
+      properties: {
+        ...feature.properties,
+        [parameter]: calc(feature),
+      },
+    };
   }
 
   // Municipality data needs calculated from postnumber data
   const values = {};
 
-  pno_stat.features.forEach((feature) => {
-    const municipalityId = feature.properties.kunta;
+  pno_stat.features
+    .filter((pno) => pno.properties.kunta === feature.properties.kunta)
+    .forEach((feature) => {
+      const municipalityId = feature.properties.kunta;
 
-    if (!values.hasOwnProperty(municipalityId)) {
-      values[municipalityId] = [feature.properties[parameter]];
-    } else {
-      values[municipalityId].push(feature.properties[parameter]);
-    }
-  });
+      if (!values.hasOwnProperty(municipalityId)) {
+        values[municipalityId] = [feature.properties[parameter]];
+      } else {
+        values[municipalityId].push(feature.properties[parameter]);
+      }
+    });
 
-  return features.map((feature) => {
-    return {
-      ...feature,
-      properties: {
-        ...feature.properties,
-        [parameter]: values[feature.properties.kunta].reduce(
-          (sum, current) => sum + current,
-          0
-        ),
-      },
-    };
-  });
+  return {
+    ...feature,
+    properties: {
+      ...feature.properties,
+      [parameter]: values[feature.properties.kunta].reduce(
+        (sum, current) => sum + current,
+        0
+      ),
+    },
+  };
 };
 
 export { preProcessData };
