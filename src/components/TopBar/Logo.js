a8a496ebe143b1f8a85bc1e6a6df4557f7c17876
viekataj <viekataj@student.jyu.fi>
2025-12-06 19:10:12 +0200

    Started working on logo
    Also minor improvements elsewhere.

--- src/components/TopBar/Logo.js
+++ src/components/TopBar/Logo.js
@@ -1,13 +1,17 @@
 import { memo } from "react";
+import Image from "next/image";
 
 function Logo() {
-
     return (
-        <div className="absolute p-[3vh] right-0 lg:right-[65vw]">
-            <h1>Ponse</h1>
+        <div className="absolute p-[1vh] right-0 max-w-[362px] max-h-[148px] w-full h-full">
+            <Image
+                src="/images/LogoDarkModePicture.png"
+                alt="Ponse"
+                width={362}
+                height={148}
+            ></Image>
         </div>
     );
-
 }
 
 export default memo(Logo);
