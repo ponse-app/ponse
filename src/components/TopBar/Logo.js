b6fa552cdced74ae360d4858d42ab8eb5aaa4488
viekataj <viekataj@student.jyu.fi>
2025-12-07 19:42:45 +0200

    Added light mode support for logo.

--- src/components/TopBar/Logo.js
+++ src/components/TopBar/Logo.js
@@ -1,14 +1,23 @@
 import { memo } from "react";
 import Image from "next/image";
+import styles from "./theme-image.module.css";
 
 function Logo() {
     return (
-        <div className="absolute p-[1vh] right-0 max-w-[362px] max-h-[148px] w-full h-full">
+        <div className="absolute p-[1vh] right-0 max-w-[362px] max-h-[148px] w-full h-full flex justify-end">
             <Image
                 src="/images/LogoDarkModePicture.png"
                 alt="Ponse"
-                width={362}
-                height={148}
+                width={227}
+                height={90}
+                className={styles.imgDark}
+            ></Image>
+            <Image
+                src="/images/LogoLightModePicture.png"
+                alt="Ponse"
+                width={239}
+                height={95}
+                className={styles.imgLight}
             ></Image>
         </div>
     );
