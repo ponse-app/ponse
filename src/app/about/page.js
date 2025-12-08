434a8549706a5fd1bcb4ea571cabf3acae254f86
Veeti Kataja <veeti.kataja@gmail.com>
2025-12-08 14:53:42 +0200

    Added github links

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -11,7 +11,23 @@ export default function Home() {
                 <h1>{t("about.title")}</h1>
                 <p>{t("about.project")}</p>
                 <h1>{t("about.we")}</h1>
-                <p>{t("about.aboutUs")}</p>
+                <p>
+                    <a href="https://github.com/juuso-turunen" className="text-blue-500">
+                        Juuso Turunen
+                    </a>
+                    ,{" "}
+                    <a href="https://github.com/paajooni" className="text-blue-500">
+                        Joonas Paasovaara
+                    </a>
+                    ,{" "}
+                    <a href="https://github.com/enxoc" className="text-blue-500">
+                        Atte Oinonen
+                    </a>
+                    ,{" "}
+                    <a href="https://github.com/Vieteri27" className="text-blue-500">
+                        Veeti Kataja
+                    </a>
+                </p>
             </main>
         </div>
     );
